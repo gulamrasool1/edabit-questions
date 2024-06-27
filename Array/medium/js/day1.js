7791dc89ea8ede1c980def084169e71693d64f63
gulamrasool1 <gulamrasul0143@gmail.com>
2024-06-27 14:47:02 +0530

    we solve Edabit question

--- Array/medium/js/day1.js
+++ Array/medium/js/day1.js
@@ -78,8 +78,8 @@ function toArray(obj) {
 
 // Examples 
 
-function concat(arr1, arr2, arr3) {
-    return arr1.concat(arr2,arr3)
+function concat(...arr) {
+    return [].concat(...arr);
 }
 
 console.log(concat([1, 2, 3], [4, 5], [6, 7]));  //➞ [1, 2, 3, 4, 5, 6, 7] 
