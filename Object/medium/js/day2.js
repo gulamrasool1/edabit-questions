415df481f947fa65e7ece69ade3ee3c096a5efb5
gulamrasool1 <gulamrasul0143@gmail.com>
2024-08-24 16:55:21 +0530

    Edabit question

--- Object/medium/js/day2.js
+++ Object/medium/js/day2.js
@@ -117,19 +117,41 @@ function solve(stolenItems, petName) {
 // 15 ES6: Destructuring Objects X
 // Given an array of user objects.
 
-let names = []
+// let names = []
 
-let users = [
-  { name: "John", email: "john@example.com" },
-  { name: "Jason", email: "jason@example.com" },
-  { name: "Jeremy", email: "jeremy@example.com" },
-  { name: "Jacob", email: "jacob@example.com" }
-]
+// let users = [
+//   { name: "John", email: "john@example.com" },
+//   { name: "Jason", email: "jason@example.com" },
+//   { name: "Jeremy", email: "jeremy@example.com" },
+//   { name: "Jacob", email: "jacob@example.com" }
+// ]
 
-for (let i = 0; i < users.length; i++) {
-    names.push(users[i].name)
-}
+// for (let i = 0; i < users.length; i++) {
+//     names.push(users[i].name)
+// }
 
-console.log(names) // should log ["John", "Jason", "Jeremy", "Jacob"]
+// console.log(names) // should log ["John", "Jason", "Jeremy", "Jacob"]
 // Push the first names of all users in the names array.
 
+
+
+// 16 Can You Spare a Square?
+// Try to imagine a world in which you might have to stay home for 14 days at any given time. Do you have enough TP to make it through?
+
+// Although the number of squares per roll of TP varies significantly, we'll assume each roll has 500 sheets, and the average person uses 57 sheets per day.
+
+// Create a function that will receive an object with two key/values:
+
+// people ⁠— Number of people in the household.
+// tp ⁠— Number of rolls.
+// Return a statement telling the user if they need to buy more TP!
+
+// Examples
+
+function tpChecker(obj) {
+    
+}
+
+console.log(tpChecker({ people: 4, tp: 1 }));  //➞ "Your TP will only last 2 days, buy more!"
+console.log(tpChecker({ people: 3, tp: 20 }));  //➞ "Your TP will last 58 days, no need to panic!"
+console.log(tpChecker({ people: 4, tp: 12 }));  //➞ "Your TP will last 26 days, no need to panic!"
