a95e4ece04cf5f40d3cab3e3c786d70ebe988abe
gulamrasool1 <gulamrasul0143@gmail.com>
2024-11-30 20:21:28 +0530

    Edabit question

--- String/medium/js/day9.js
+++ String/medium/js/day9.js
@@ -42,7 +42,7 @@ function isHarshad(num) {
 // }
 
 function capSpace(str) {
-    return str.split('').map(char => char !== char.toLowerCase() ? " " + char.toLowerCase(): char.toLowerCase()).join("");
+    return str.split('').map(char => char !== char.toLowerCase() ? " " + char.toLowerCase() : char.toLowerCase()).join("");
 }
 
 // console.log(capSpace("helloWorld"));  //➞ "hello world"
@@ -65,7 +65,7 @@ function doubleton(num) {
         let digits = new Set(String(i));
         if (digits.size === 2) {
             return i;
-        }     
+        }
     }
 }
 
@@ -126,7 +126,7 @@ function calculateScores(str) {
         } else if (str[i] === 'B') {
             scores[1]++;
         } else if (str[i] === 'C') {
-            scores[2]++; 
+            scores[2]++;
         }
     }
     return scores;
@@ -163,6 +163,98 @@ function firstNVowels(str, num) {
     return vowelsArray.length >= num ? vowelsArray.slice(0, num).join("") : "invalid";
 }
 
-console.log(firstNVowels("sharpening skills", 3));  //➞ "aei"
-console.log(firstNVowels("major league", 5));  //➞ "aoeau"
-console.log(firstNVowels("hostess", 5));  //➞ "invalid"
+// console.log(firstNVowels("sharpening skills", 3));  //➞ "aei"
+// console.log(firstNVowels("major league", 5));  //➞ "aoeau"
+// console.log(firstNVowels("hostess", 5));  //➞ "invalid"
+
+
+
+// 87 First Before Second Letter
+// You are given three inputs: a string, one letter, and a second letter.
+
+// Write a function that returns true if every instance of the first letter occurs before every instance of the second letter.
+
+// Examples
+
+// function firstBeforeSecond(str, first, second) {
+//     let result = false;
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === second) {
+//             result = true
+//         } else if (result && str[i] === first) {
+//             return false
+//         }
+//     }
+//     return true;
+// }
+
+function firstBeforeSecond(str, first, second) {
+    let lastIndex = str.lastIndexOf(first);
+    let firstIndex = str.indexOf(second);
+    return lastIndex < firstIndex;
+}
+// console.log(firstBeforeSecond("a rabbit jumps joyfully", "a", "j"));  //➞ true
+// every instance of "a" occurs before every instance of "j"
+// console.log(firstBeforeSecond("knaves knew about waterfalls", "k", "w"));  //➞  true
+// console.log(firstBeforeSecond("happy birthday", "a", "y"));  //➞ false
+// the "a" in "birthday" occurs after the "y" in "happy"
+// console.log(firstBeforeSecond("precarious kangaroos", "k", "a"));  //➞ false
+
+
+
+// 88 Return the Index of All Capital Letters
+// Create a function that takes a single string as argument and returns an ordered array containing the indices of all capital letters in the string.
+
+// Examples
+
+// function indexOfCaps(str) {
+//     let result = [];
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] >= 'A' && str[i] <= 'Z') {
+//             result.push(i);
+//         }
+//     }
+//     return result
+// }
+
+function indexOfCaps(str) {
+    return [...str].map((char, i) => char >= 'A' && char <= 'Z' ? i : -1).filter(i => i !== -1);
+}
+
+// console.log(indexOfCaps("eDaBiT"));  //➞ [1, 3, 5]
+// console.log(indexOfCaps("eQuINoX"));  //➞ [1, 3, 4, 6]
+// console.log(indexOfCaps("determine"));  //➞ []
+// console.log(indexOfCaps("STRIKE"));  //➞ [0, 1, 2, 3, 4, 5]
+// console.log(indexOfCaps("sUn"));  //➞ [1]
+
+
+
+// 89 Longest Sequence of Consecutive Zeroes
+// Write a function that returns the longest sequence of consecutive zeroes in a binary string.
+
+// Examples
+
+function longestZero(str) {
+    let splited = str.split('1')
+    let longest = splited.reduce((max, item) => max.length > item.length ? max : item)
+    return longest
+}
+
+// console.log(longestZero("01100001011000"));  //➞ "0000"
+// console.log(longestZero("100100100"));  //➞ "00"
+// console.log(longestZero("11111"));  //➞ ""
+
+
+
+// 90 Capitalize the Last Letter
+// Create a function that capitalizes the last letter of every word.
+
+// Examples
+
+function capLast(str) {
+    return str.split(" ").map(word => word.slice(0, -1) + word.slice(-1).toUpperCase()).join(" ");
+}
+
+console.log(capLast("hello"));  //➞ "hellO"
+console.log(capLast("My Name Is Edabit"));  //➞ "MY NamE IS EdabiT"
+console.log(capLast("HELp THe LASt LETTERs CAPITALISe"));  //➞ "HELP THE LAST LETTERS CAPITALISE"
