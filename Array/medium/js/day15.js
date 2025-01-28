f762a22c95be4cbc41a7767a364233573a838d71
gulamrasool1 <gulamrasul0143@gmail.com>
2025-01-28 21:08:52 +0530

    Edabit question

--- Array/medium/js/day15.js
+++ Array/medium/js/day15.js
@@ -95,4 +95,4 @@ function billSplit(arr1, arr2) {
 console.log(billSplit(["N", "S", "N"], [10, 10, 20]));  //➞ [25, 15]
 // You pay for half of both "N" dishes (5 + 10) and entirely pay for the "S" dish (10).
 console.log(billSplit(["N", "N"], [10, 10]));  //➞ [10, 10]
-console.log(billSplit(["S", "N"], [41, 10]))  //➞ [46, 5]
+console.log(billSplit(["S", "N"], [41, 10]));  //➞ [46, 5]
