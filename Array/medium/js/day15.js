42588a754ac39c8e68330553e5843a224bc20fa1
gulamrasool1 <gulamrasul0143@gmail.com>
2025-02-19 21:13:17 +0530

    Edabit question

--- Array/medium/js/day15.js
+++ Array/medium/js/day15.js
@@ -156,6 +156,6 @@ function getProducts(arr) {
     return arr.map((_, i) => arr.reduce((acc, num, j) => i !== j ? acc * num : acc, 1));
 }
 
-console.log(getProducts([1, 7, 3, 4]));   //➞ [84, 12, 28, 21]
+console.log(getProducts([1, 7, 3, 4]));  //➞ [84, 12, 28, 21]
 console.log(getProducts([1, 2, 6, 5, 9]));  //➞ [540, 270, 90, 108, 60]
 console.log(getProducts([1, 2, 3, 0, 5]));  //➞ [0, 0, 0, 30, 0]
